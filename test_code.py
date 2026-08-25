d61a7ca217632b4179610fca2c92321c5fda85af
Ezra Muhizi <muhiziezra008@gmail.com>
2026-08-25 09:55:52 +0200

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,4 +1,5 @@
 def login(username, password):
     query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
     print(password)
+    print("Testing automatic review")
     return query
