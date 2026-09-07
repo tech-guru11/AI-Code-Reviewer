fd4b3811c20865bc405879d3997c38f7d2445bd3
Ezra Muhizi <muhiziezra008@gmail.com>
2026-09-07 13:58:28 +0200

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -27,3 +27,11 @@ def search_user(username):
 def search_user(username):
     query = " * FROM users WHERE username='" + username + "'"
     return query
+def test_function():
+    print("new commit test")
+def search_use):
+    query = "SELECT * FROM users WHERE username='" + username + "'"
+    return query
+def search_user(username):
+    query = " * FROM users WHERE username='" + username + "'"
+    return query
